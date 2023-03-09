229dfd9feda7f1b89c50fbb02391cb6b9c77d547
Muthu <muthukumar.m@DT150157.QICHOAPPL.com>
2023-03-09 20:19:22 +0530

    Initial Code Commit.........

---
+++ flutter-training/src/main/java/com/spring/basic/FlutterTrainingApplication.java
@@ -0,0 +1,18 @@
+package com.spring.basic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author MayilaiMuthu
+ * @apiNote 09-03-2023
+ *
+ */
+@SpringBootApplication
+public class FlutterTrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlutterTrainingApplication.class, args);
+	}
+
+}
